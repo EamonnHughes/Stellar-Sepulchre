77778da27b5da8ba84dac765289efb5537a2791a
Eamonn Hughes <efohughes@gmail.com>
2023-12-17 18:02:43 -0500

    fixes

--- core/src/org/eamonn/trog/package.scala
+++ core/src/org/eamonn/trog/package.scala
@@ -45,7 +45,8 @@ package object trog {
     }
   }
 
-  def screenUnit: Float = (Geometry.ScreenWidth min Geometry.ScreenHeight) / 15
+  // 16 should match desktop launcher
+  def screenUnit: Float = (Geometry.ScreenWidth min Geometry.ScreenHeight) / 16
   def textUnit: Float = (Geometry.ScreenWidth min Geometry.ScreenHeight) / 20
 
   def compassAvailable: Boolean =
